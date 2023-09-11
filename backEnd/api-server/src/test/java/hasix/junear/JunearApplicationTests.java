65519f6cefb0cef067d42da3e54376584161f500
JoInHyeok <ss112d86@gmail.com>
2023-09-11 15:37:56 +0900

    FEAT: backend spring project init

---
+++ backEnd/api-server/src/test/java/hasix/junear/JunearApplicationTests.java
@@ -0,0 +1,13 @@
+package hasix.junear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
