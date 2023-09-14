a24c70f76fc7500a53f4d478b4b12a5675964b88
최홍준 <choi12920@naver.com>
2023-09-14 16:38:20 +0900

    FEATURE<Client>: oauth login implement
    Merge: Merge branch 'feature/login-oauth' into 'develop'
    
    * FEAT: login oauth
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!6

--- frontEnd/junear/src/App.tsx
+++ frontEnd/junear/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import NavBar from 'components/NavBar';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from 'pages/member/LoginPage';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
