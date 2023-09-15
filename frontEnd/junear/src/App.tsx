347098d99841c29a6a87cbd068e4ef44a172b0af
HBumzz <choi12920@naver.com>
2023-09-15 16:00:27 +0900

    FEAT: implement login component

--- frontEnd/junear/src/App.tsx
+++ frontEnd/junear/src/App.tsx
@@ -2,11 +2,17 @@ import './App.css';
 import NavBar from 'components/NavBar';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from 'pages/member/LoginPage';
+import MyPage from 'pages/member/MyPage';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="grid grid-cols-12 gap-[25px] mx-[200px]">
+        <Routes>
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
       </Routes>
