2f5ec3250b12b0fc0d6ef4e7c2232fb0e4547884
Ha Jung Ho <ggk14@naver.com>
2023-09-18 16:57:44 +0900

    FEAT: add Corporation Entity

--- backEnd/api-server/src/main/java/hasix/junear/corporation/domain/Corporation.java
+++ backEnd/api-server/src/main/java/hasix/junear/corporation/domain/Corporation.java
@@ -1,5 +1,47 @@
 package hasix.junear.corporation.domain;
 
-public class Corporation {
+import hasix.junear.common.model.BaseTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Corporation extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "corporation_id")
+    private Long id;
+
+    @Column(name = "corporation_code")
+    private Long corporationCode;
+
+    @Column
+    private String name;
+
+    @Column(name = "stability_rank")
+    private Long stabilityRank;
+
+    @Column(name = "growth_rank")
+    private Long growthRank;
+
+    @Column(name = "profitability_rank")
+    private Long profitabilityRank;
+
+    @Column(name = "activity_rank")
+    private Long activityRank;
+
+    @Column(name = "total_rank")
+    private Double totalRank;
+
+    @Column(name = "stock_close")
+    private Long stockClose;
 
 }
