8fc51bcb124344630f9502b91071ae8e6b0fbe78
송준우 <boaz42@khu.ac.kr>
2023-09-18 14:38:53 +0900

    FEATURE<Client>: create portfolio page publishing
    Merge: Merge branch 'feature/mainpage' into 'develop'
    
    * FEAT: publishing create portfolio page
    
    * FEAT: add rank image
    
    * FEAT: add mainpage grid layout
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!9

--- frontEnd/junear/src/App.tsx
+++ frontEnd/junear/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css';
-import NavBar from 'components/NavBar';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import NavBar from 'components/common/NavBar';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from 'pages/member/LoginPage';
+import HomePage from 'pages/main/HomePage';
+import CreatePort from 'pages/Portfolio/CreatePort';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <div className="grid grid-cols-12 gap-[25px] mx-[200px]">
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/portfolio" element={<CreatePort />} />
+        </Routes>
+      </div>
     </div>
   );
 }
