141d38ffe93fc27d383a8c4d1a4bf04fb7edf615
송준우 <boaz42@khu.ac.kr>
2023-09-19 17:53:15 +0900

    FEAT: add create portfolio modal

--- frontEnd/junear/src/pages/Portfolio/CreatePort.tsx
+++ frontEnd/junear/src/pages/Portfolio/CreatePort.tsx
@@ -1,4 +1,20 @@
+import AddPortInfo from 'components/modal/AddPortInfo';
+import { useState } from 'react';
+
 function CreatePort() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const handleModalBackgroundClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
   return (
     <>
       <div className="col-span-12 text-white mt-[125px] text-[28px] text-center">보유중인 주식을 추가해 주십시오.</div>
@@ -7,12 +23,23 @@ function CreatePort() {
       <div className="col-start-8 col-end-13 h-[420px] bg-zinc-700 rounded-[20px]"></div>
       <div className="col-span-12 mt-[5px]"></div>
       <div className="relative col-span-12">
-        <button className="absolute text-white bg-teal-500 rounded-[20px] p-[14px] w-[250px] whitespace-nowrap left-[430px]">
+        <button
+          className="absolute text-white bg-teal-500 rounded-[20px] p-[14px] w-[250px] whitespace-nowrap left-[430px]"
+          onClick={openModal}
+        >
           포트폴리오 생성하기
         </button>
       </div>
 
       <div className="col-span-12 mt-[80px]"></div>
+      {isModalOpen && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleModalBackgroundClick}
+        >
+          <AddPortInfo onClose={closeModal} />
+        </div>
+      )}
     </>
   );
 }
--- frontEnd/junear/src/pages/Portfolio/MyPort.tsx
+++ frontEnd/junear/src/pages/Portfolio/MyPort.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { ReactComponent as Filter } from '../../assets/image/filter.svg';
 import { ReactComponent as Plus } from '../../assets/image/plus.svg';
+import { ReactComponent as Srank } from '../../assets/image/Srank.svg';
+import AbstractChart from 'components/portfolio/AbstractChart';
 
 function MyPort() {
   const [activeButton, setActiveButton] = useState('등급');
@@ -39,10 +41,25 @@ function MyPort() {
               금액
             </button>
           </div>
+          <AbstractChart />
           {/* 여기에 차트가 들어가야함 등급일땐 등급관한거 금액일땐 금액관한거 랜더링 */}
         </div>
         <div className="h-[25px]"></div>
-        <div className="h-[300px] bg-zinc-700 rounded-[20px]"></div>
+        <div className="h-[300px] bg-zinc-700 rounded-[20px]">
+          <div className="pt-[25px] text-zinc-400">내 포트폴리오 지표</div>
+          <div className="flex gap-[20px] justify-center mt-[50px]">
+            <Srank />
+            <Srank />
+            <Srank />
+            <Srank />
+          </div>
+          <div className="flex gap-[92px] text-white justify-center mt-[10px]">
+            <span>안정성</span>
+            <span>성장성</span>
+            <span>수익성</span>
+            <span>활동성</span>
+          </div>
+        </div>
       </div>
       <div className="col-start-7 col-end-13 h-[925px] mt-[100px] bg-zinc-700 rounded-[20px]">
         <div className="flex content-center justify-end gap-[15px] pr-[25px] mt-[25px]">
