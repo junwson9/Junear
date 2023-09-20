ac2d84a40f90e67383dad0fcd28c2ce0134badec
JoInHyeok <ss112d86@gmail.com>
2023-09-20 13:53:45 +0900

    REFACTOR: remove oauth api, integrate auth api

--- backEnd/api-server/src/main/java/hasix/junear/member/api/AuthApi.java
+++ backEnd/api-server/src/main/java/hasix/junear/member/api/AuthApi.java
@@ -2,12 +2,21 @@ package hasix.junear.member.api;
 
 
 import hasix.junear.common.response.ResponseFactory;
+import hasix.junear.member.api.dto.OauthLoginApiRequest;
+import hasix.junear.member.api.dto.OauthLoginApiResponse;
 import hasix.junear.member.application.LogoutUseCase;
+import hasix.junear.member.application.OauthLoginUseCase;
+import hasix.junear.member.application.dto.OauthLoginResponse;
+import hasix.junear.member.domain.Token;
 import hasix.junear.springconfig.config.auth.AuthMember;
 import hasix.junear.springconfig.config.auth.AuthenticatedMember;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,10 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthApi {
 
     private final LogoutUseCase logoutUseCase;
+    private final OauthLoginUseCase oauthLoginUseCase;
+
 
     @GetMapping("/api/member/logout")
     public ResponseEntity<?> logout(@AuthenticatedMember AuthMember member){
         logoutUseCase.logout(member.getId());
         return ResponseFactory.success("로그아웃 성공");
+    }
+
+    @PostMapping("/api/member/oauth")
+    public ResponseEntity<?> oauthLogin(@Validated @RequestBody OauthLoginApiRequest request, HttpServletResponse response){
+        OauthLoginResponse result = oauthLoginUseCase.login(request.toApplicationDto());
+        Token accessToken = result.getAccessToken();
+        Token refreshToken = result.getRefreshToken();
+        response.addCookie(refreshToken.createCookie());
+        return ResponseFactory.success("로그인 성공", new OauthLoginApiResponse(accessToken.getToken()));
     }
 }
