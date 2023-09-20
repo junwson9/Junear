a23d8ca4df1064099dfcd0ca81ea75c857553e99
최홍준 <choi12920@naver.com>
2023-09-20 09:36:24 +0900

    FEATURE<Client>: all components implement
    Merge: Merge branch 'FE/feature/news' into 'develop'
    
    * FEAT: all components implement
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!16

---
+++ frontEnd/junear/src/components/common/Bookmark.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactComponent as BookMarkIcon } from '../../assets/image/bookmark-noclick.svg';
+import { ReactComponent as BookMarkIconClick } from '../../assets/image/bookmark-click.svg';
+
+type BookMarkProps = {
+  isBookmarked: boolean;
+  onClick: () => void;
+};
+
+function BookMark({ isBookmarked, onClick }: BookMarkProps) {
+  // isBookmarked 값에 따라 아이콘을 표시하거나 숨깁니다.
+  return isBookmarked ? <BookMarkIconClick onClick={onClick} /> : <BookMarkIcon onClick={onClick} />;
+}
+
+export default BookMark;
