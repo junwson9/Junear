f39f4b1b6ac9bcb45ba1842135be72652e1ed064
하정호 <ggk14@naver.com>
2023-09-22 17:31:43 +0900

    REFACTOR: Change SearchCorporationKeyword Service Logic
    Merge: Merge branch 'BE/feature/corporation' into 'develop'
    
    * REFACTOR: change service logic
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!21

--- backEnd/api-server/src/main/java/hasix/junear/corporation/domain/CorporationRepository.java
+++ backEnd/api-server/src/main/java/hasix/junear/corporation/domain/CorporationRepository.java
@@ -1,5 +1,6 @@
 package hasix.junear.corporation.domain;
 
+import hasix.junear.corporation.application.dto.SearchCorporationKeywordResponse;
 import hasix.junear.corporation.application.dto.ViewCorporationDetailsResponse;
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +9,8 @@ public interface CorporationRepository {
 
     Optional<ViewCorporationDetailsResponse> findViewCorporationDetailsResponseById(Long id);
 
-    List<Corporation> findByNameIsContaining(String keyword);
+    List<SearchCorporationKeywordResponse> findByNameIsContaining(String keyword);
 
-    List<Corporation> findByCorporationCodeIsContaining(String keyword);
+    List<SearchCorporationKeywordResponse> findByCorporationCodeIsContaining(String keyword);
 
 }
--- backEnd/api-server/src/main/java/hasix/junear/corporation/infra/mysql/JpaCorporationRepository.java
+++ backEnd/api-server/src/main/java/hasix/junear/corporation/infra/mysql/JpaCorporationRepository.java
@@ -1,5 +1,6 @@
 package hasix.junear.corporation.infra.mysql;
 
+import hasix.junear.corporation.application.dto.SearchCorporationKeywordResponse;
 import hasix.junear.corporation.application.dto.ViewCorporationDetailsResponse;
 import hasix.junear.corporation.domain.Corporation;
 import java.util.List;
@@ -12,8 +13,10 @@ public interface JpaCorporationRepository extends JpaRepository<Corporation, Lon
     @Query("SELECT new hasix.junear.corporation.application.dto.ViewCorporationDetailsResponse(c.id, c.industryId, i.type, c.corporationCode, c.name, c.stabilityRank, c.growthRank, c.profitabilityRank, c.activityRank, c.totalRank, c.stockClose) FROM Corporation c LEFT JOIN Industry i ON c.industryId = i.id WHERE c.id = :id")
     Optional<ViewCorporationDetailsResponse> findViewCorporationDetailsResponseById(Long id);
 
-    List<Corporation> findByNameIsContaining(String keyword);
+    @Query("SELECT new hasix.junear.corporation.application.dto.SearchCorporationKeywordResponse(c.id, i.type, c.name) FROM Corporation c LEFT JOIN Industry i ON c.industryId = i.id WHERE c.name LIKE CONCAT('%', :keyword, '%')")
+    List<SearchCorporationKeywordResponse> findByNameIsContaining(String keyword);
 
-    List<Corporation> findByCorporationCodeIsContaining(String keyword);
+    @Query("SELECT new hasix.junear.corporation.application.dto.SearchCorporationKeywordResponse(c.id, i.type, c.name) FROM Corporation c LEFT JOIN Industry i ON c.industryId = i.id WHERE c.corporationCode LIKE CONCAT('%', :keyword, '%')")
+    List<SearchCorporationKeywordResponse> findByCorporationCodeIsContaining(String keyword);
 
 }
