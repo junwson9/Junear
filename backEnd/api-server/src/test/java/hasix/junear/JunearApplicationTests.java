f36f612e33225c9bbd98cc5c50303c5904f2302f
JoInHyeok <ss112d86@gmail.com>
2023-09-22 12:58:57 +0900

    FIX: remove integeration test code

--- backEnd/api-server/src/test/java/hasix/junear/JunearApplicationTests.java
+++ backEnd/api-server/src/test/java/hasix/junear/JunearApplicationTests.java
@@ -3,10 +3,10 @@ package hasix.junear;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JunearApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
