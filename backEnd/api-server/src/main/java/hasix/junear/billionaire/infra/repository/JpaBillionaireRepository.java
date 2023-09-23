96a17aef6ed7de53d916ed3a7a6374f3298e3710
jlal1226 <jlal1226@naver.com>
2023-09-23 17:05:32 +0900

    REFACTOR: remove billionaire native query

--- backEnd/api-server/src/main/java/hasix/junear/billionaire/infra/repository/JpaBillionaireRepository.java
+++ backEnd/api-server/src/main/java/hasix/junear/billionaire/infra/repository/JpaBillionaireRepository.java
@@ -1,16 +1,9 @@
 package hasix.junear.billionaire.infra.repository;
 
-import hasix.junear.billionaire.application.dto.TodayLifeQuotes;
 import hasix.junear.billionaire.domain.Billionaire;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface JpaBillionaireRepository extends JpaRepository<Billionaire, Long> {
 
-    // JPQL join 쿼리
-    @Query(value = "SELECT b.name, b.image_url, l.phrase FROM billionaire b JOIN life_quotes l ON b.billionaire_id = l.billionaire_id WHERE l.billionaire_id IN :ids", nativeQuery = true)
-    List<Object> findBillionaireLifeQuotesByIds(@Param("ids") List<Long> ids);
+    
 }
