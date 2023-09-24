86c7eae5a3017f5309dc41baa7c6a64219ffe58c
Ha Jung Ho <ggk14@naver.com>
2023-09-25 03:02:30 +0900

    FEAT: implement portfolioCreate

---
+++ backEnd/api-server/src/main/java/hasix/junear/portfolio/application/dto/CreatePortfolioRequest.java
@@ -0,0 +1,50 @@
+package hasix.junear.portfolio.application.dto;
+
+import hasix.junear.portfolio.api.dto.PortFolioAddApiRequest;
+import hasix.junear.portfolio.domain.Portfolio;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CreatePortfolioRequest {
+
+    private Long memberId;
+    private Long corporationId;
+    private Long stockCount;
+    private Long averagePrice;
+
+    @Builder
+    public CreatePortfolioRequest(Long memberId, Long corporationId, Long stockCount,
+            Long averagePrice) {
+        this.memberId = memberId;
+        this.corporationId = corporationId;
+        this.stockCount = stockCount;
+        this.averagePrice = averagePrice;
+    }
+
+    public static List<CreatePortfolioRequest> from(Long memberId,
+            List<PortFolioAddApiRequest> requestList) {
+
+        return requestList.stream()
+                          .map(request -> CreatePortfolioRequest.builder()
+                                                                .memberId(memberId)
+                                                                .corporationId(request.getCorporation_id())
+                                                                .stockCount(request.getStock_count())
+                                                                .averagePrice(request.getAverage_price())
+                                                                .build())
+                        .collect(Collectors.toList());
+    }
+
+    public static Portfolio toPortfolio (CreatePortfolioRequest request){
+
+        return Portfolio.builder()
+                        .memberId(request.getMemberId())
+                        .corporationId(request.getCorporationId())
+                        .stockCount(request.getStockCount())
+                        .averagePrice(request.getAveragePrice())
+                        .build();
+    }
+
+}
