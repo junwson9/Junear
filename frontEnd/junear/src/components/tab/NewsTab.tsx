348bf52f15be09e8f96d8e75166632345e005174
HBumzz <choi12920@naver.com>
2023-09-25 15:43:25 +0900

    FIX: newstab fix

--- frontEnd/junear/src/components/tab/NewsTab.tsx
+++ frontEnd/junear/src/components/tab/NewsTab.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { useEffect } from 'react';
+import axiosInstance from 'state/AxiosInterceptor';
 
 export default function NewsTab() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(1);
   const category_str = [
     '전기,전자',
     '건설업',
@@ -26,7 +28,18 @@ export default function NewsTab() {
     '유통업',
     '전기가스업',
   ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get('/news?industry_id=?');
+        console.log(response.data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    fetchData();
+  }, [value]);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     // newValue 값을 검증하여 0부터 18 사이의 값으로 제한합니다.
     if (newValue >= 0 && newValue < category_str.length) {
