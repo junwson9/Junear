59071d2dba68d35100b5399e7b6951e6f1454774
송준우 <boaz42@khu.ac.kr>
2023-09-26 13:38:01 +0900

    FEATURE<Client>: add millionaireCard
    Merge: Merge branch 'FE/feature/millionaireCard' into 'develop'
    
    * FEAT: add millionaireCard
    
    * FIX: fix navbar console error
    
    * FIX: navbar key error
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!30

--- frontEnd/junear/src/pages/main/HomePage.tsx
+++ frontEnd/junear/src/pages/main/HomePage.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import Card from '../../components/main/Card';
-
+import axiosInstance from 'state/AxiosInterceptor';
+interface Millionaire {
+  name: string;
+  image_url: string;
+  phrase: string;
+}
+interface ApiResponse {
+  message: string;
+  data: Millionaire[];
+}
 function HomePage() {
+  const [millionaireData, setMillionaireData] = useState<Millionaire[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get<ApiResponse>('/billionaire/today');
+        setMillionaireData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <div className="col-span-3 mt-[125px]">
@@ -18,36 +41,37 @@ function HomePage() {
       <div className="col-span-12 mt-[30px]">
         <p className="text-white font-bold text-[28px] text-left nowrap">오늘의 억만장자 명언</p>
       </div>
-      <div className="col-start-1 col-end-5 h-[350px] mt-[30px]">
-        <Card
-          front={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Front</div>
-          }
-          back={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Back</div>
-          }
-        />
-      </div>
-      <div className="col-start-5 col-end-9 h-[350px] mt-[30px]">
-        <Card
-          front={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Front</div>
-          }
-          back={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Back</div>
-          }
-        />
-      </div>
-      <div className="col-start-9 col-end-13 h-[350px] mt-[30px]">
-        <Card
-          front={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Front</div>
-          }
-          back={
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>Back</div>
-          }
-        />
-      </div>
+      {millionaireData.map((item, index) => (
+        <div
+          key={index}
+          className={`col-start-${1 + index * 4} col-end-${5 + index * 4} h-[350px]  w-[300px] mt-[30px]`}
+        >
+          <Card
+            front={
+              <div
+                className=" group relative hover:shadow-lg"
+                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+              >
+                <img src={item.image_url} alt={item.name} className="h-[350px] w-[300px] rounded-[20px]" />
+                <div className="absolute top-[150px] left-0 w-full h-full flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ">
+                  {item.name}
+                </div>
+              </div>
+            }
+            back={
+              <div
+                className="px-4"
+                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+              >
+                <div>
+                  <p>{item.phrase}</p>
+                  <p className="mt-[30px]">-{item.name}-</p>
+                </div>
+              </div>
+            }
+          />
+        </div>
+      ))}
 
       <div className="col-span-12 mt-[200px]"></div>
     </>
