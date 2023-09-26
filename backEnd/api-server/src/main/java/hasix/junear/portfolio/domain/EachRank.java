5df9aa05e9174233fe9767f7d5c53b80b2db6df0
Ha Jung Ho <ggk14@naver.com>
2023-09-26 15:19:20 +0900

    REFACTOR: refactor EachRank

--- backEnd/api-server/src/main/java/hasix/junear/portfolio/domain/EachRank.java
+++ backEnd/api-server/src/main/java/hasix/junear/portfolio/domain/EachRank.java
@@ -2,8 +2,12 @@ package hasix.junear.portfolio.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class EachRank {
 
     private Long s;
@@ -13,9 +17,10 @@ public class EachRank {
     private Long b;
     private Long cPlus;
     private Long c;
+    private Long totalCount;
 
     @Builder
-    public EachRank(Long s, Long aPlus, Long a, Long bPlus, Long b, Long cPlus, Long c) {
+    public EachRank(Long s, Long aPlus, Long a, Long bPlus, Long b, Long cPlus, Long c, Long totalCount) {
         this.s = s;
         this.aPlus = aPlus;
         this.a = a;
@@ -23,5 +28,6 @@ public class EachRank {
         this.b = b;
         this.cPlus = cPlus;
         this.c = c;
+        this.totalCount = totalCount;
     }
 }
