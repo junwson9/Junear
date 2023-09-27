0d3cff556ba4c74f68d1fd7e1b2760f1b51c6367
Ha Jung Ho <ggk14@naver.com>
2023-09-27 13:08:23 +0900

    REFACTOR: Add PortfolioId RemovePortfolioAPI

--- backEnd/api-server/src/main/java/hasix/junear/portfolio/application/dto/RemoveEachPortfolioRequest.java
+++ backEnd/api-server/src/main/java/hasix/junear/portfolio/application/dto/RemoveEachPortfolioRequest.java
@@ -6,20 +6,31 @@ import lombok.Getter;
 @Getter
 public class RemoveEachPortfolioRequest {
 
-    private Long memberId;
-    private Long corporationId;
+    //    private Long memberId;
+//    private Long corporationId;
+    private Long portfolioId;
+
+//    @Builder
+//    public RemoveEachPortfolioRequest(Long memberId, Long corporationId) {
+//        this.memberId = memberId;
+//        this.corporationId = corporationId;
+//    }
 
     @Builder
-    public RemoveEachPortfolioRequest(Long memberId, Long corporationId) {
-        this.memberId = memberId;
-        this.corporationId = corporationId;
+    public RemoveEachPortfolioRequest(Long portfolioId) {
+        this.portfolioId = portfolioId;
     }
 
-    public static RemoveEachPortfolioRequest from(Long memberId, Long corporationId) {
+    //    public static RemoveEachPortfolioRequest from(Long memberId, Long corporationId) {
+    public static RemoveEachPortfolioRequest from(Long portfolioId) {
+
+//        return RemoveEachPortfolioRequest.builder()
+//                                         .memberId(memberId)
+//                                         .corporationId(corporationId)
+//                                         .build();
 
         return RemoveEachPortfolioRequest.builder()
-                                         .memberId(memberId)
-                                         .corporationId(corporationId)
+                                         .portfolioId(portfolioId)
                                          .build();
     }
 }
