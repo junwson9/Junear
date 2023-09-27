a42bcc3fc2ac7acda9c0ad29c4f929379cb2b7cd
kimjunhyun <idkim8671@gmail.com>
2023-09-27 14:40:20 +0900

    REFACTOR: News-server refactor

--- backEnd/newscrawl-server/app/api/news.py
+++ backEnd/newscrawl-server/app/api/news.py
@@ -1,35 +1,29 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from db import SessionLocal, engine
-from models.news import News as models
-from dotenv import load_dotenv
-from bs4 import BeautifulSoup
+import sched
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime,timedelta
 import requests
 import pandas as pd
-from datetime import datetime, timedelta
-from pydantic import BaseModel
-from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+from bs4 import BeautifulSoup
 import re
 import time
+from db import SessionLocal, engine, init_db
+from sqlalchemy.orm import sessionmaker
+from models.news import News
+import logging
+import logging.config
 
-router = APIRouter()
+logging.config.fileConfig('log.ini')
 
-# Load environment variables from .env file
-load_dotenv()
+logger = logging.getLogger(__name__)
 
-# Define the NewsCreate model for request data validation
-class NewsCreate(BaseModel):
-    news_title: str
-    news_url: str
-    news_image_url: str = None
-    news_type: str
-    news_times: datetime
-    news_media: str
+sched = BackgroundScheduler(timezone="Asia/Seoul")
 
-# Function to scrape news and store it in the database
-def scrape_and_store_news(db: Session, news_item: NewsCreate):
-    table_name = 'news'
-
+@sched.scheduled_job('interval',hours=1,id='news_crawl')
+def scrape_and_store_news_scheduler():
+    # DB 실행
+    init_db()
+    
     keywords = [
         '전기,전자', '건설업', '운수장비', '의료정밀', '섬유,의복',
         '통신업', '제조업', '의약품', '서비스업', '화학', '비금속광물',
@@ -39,206 +33,40 @@ def scrape_and_store_news(db: Session, news_item: NewsCreate):
     end_date = datetime.now().strftime("%Y%m%d")
     start_date = (datetime.now() - timedelta(days=3)).strftime("%Y%m%d")
     df_tot = pd.DataFrame()
-
+    j = 1
     for i in keywords:
         search_query = i
 
         url = f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_query}&sort=RELEVANT&ds={start_date}&de={end_date}"
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
-
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        if response.status_code == 200:
-            df = pd.DataFrame()
-            news_data = []
-            news_type = [search_query] * 10
-            # BeautifulSoup을 사용하여 HTML 파싱을 수행합니다.
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            # "group_news" div 태그를 찾습니다.
-            group_news_div = soup.find('div', 'group_news')
-            news_medias = group_news_div.find_all('a','info press')
-            news_times = group_news_div.find_all('span','info')
-
-            news_medias_list = []
-            news_times_list = []
-            
-            for news_media in news_medias:
-                x = news_media.text.strip()
-                news_medias_list.append(x)
-
-            for news_time in news_times:
-                x = news_time.text.strip()
-                if x[-1] != '전':
-                    if x[0:4] == '2023':
-                        news_times_list.append(x)
-                    else:
-                        pass
-                else:
-                    news_times_list.append(x)
-            
-            current_date = datetime.now()
-
-            # 변환된 결과를 저장할 리스트를 초기화합니다.
-            converted_dates = []
-            d = news_times_list
-
-            # 주어진 문자열 리스트를 순회하면서 datetime으로 변환합니다.
-            for date_str in d:
-                if '전' in date_str:  # "3일 전" 또는 "3시간 전" 형식인 경우
-                    date_split = date_str.split()
-                    match = re.search(r'\d+', date_str)  # 숫자 패턴을 찾습니다.
-                    if match:
-                        time_value = int(match.group())  # 숫자를 추출하고 정수로 변환합니다.
-                    print(time_value)  # 출력: 3
-                    # 시간 단위와 값 추출
-
-                    match = re.search(r'(시간|일)', date_split[0])  # "시간" 또는 "일" 패턴을 찾습니다.
-                    if match:
-                        time_unit = match.group()  # "시간" 또는 "일"을 추출합니다.
-                        print(time_unit)  # 출력: "시간"
-
-                    if time_unit == '일':
-                        converted_date = current_date - timedelta(days=time_value)
-                    elif time_unit == '시간':
-                        converted_date = current_date - timedelta(hours=time_value)
-                    else:
-                        # 처리하지 않은 다른 시간 단위에 대한 로직을 추가하세요.
-                        # 예를 들어, '분' 또는 '주' 등을 처리할 수 있습니다.
-                        converted_date = None
-
-                    if converted_date:
-                        converted_dates.append(converted_date)
-                else:  # "2023.09.01."과 같은 형식인 경우
-                    try:
-                        converted_date = datetime.strptime(date_str, "%Y.%m.%d.")
-                        converted_dates.append(converted_date)
-                    except ValueError:
-                        # 날짜 형식이 잘못된 경우 처리할 수 있습니다.
-                        pass
-
-            news_times_list = converted_dates
-
-            # 뉴스 리스트를 포함한 하위 요소들을 가져옵니다.
-            news_items = group_news_div.find_all('a','news_tit')
-            # print("=====")
-            # print(news_items)
-            for news_item in news_items:
-                # 기사 제목 추출
-                news_title = news_item.text.strip()
-
-                # 기사 URL 추출
-                news_url = news_item['href']
-
-                # 이미지 데이터 추출
-                news_image = news_item.find_previous('img', class_='thumb api_get')
-                if news_image:
-                    news_image_url = news_image.get('data-lazysrc')
-                else:
-                    news_image_url = None
-
-                # 결과를 딕셔너리로 저장하고 리스트에 추가합니다.
-                news_item_data = {
-                    'title': news_title,
-                    'url': news_url,
-                    'image_url': news_image_url
-                }
-                news_data.append(news_item_data)
-            
-            news_titles = []
-            news_urls = []
-            news_image_urls = []
-            # 추출된 뉴스 데이터 출력 또는 활용
-            for news_item in news_data:
-                news_titles.append(news_item['title'])
-                news_urls.append(news_item['url'])
-                news_image_urls.append(news_item['image_url'])
-
-            df['news_title'] = news_titles
-            df['news_url'] = news_urls
-            df['news_image_url'] = news_image_urls
-            df['news_type'] = [search_query] * 10
-            df['news_times'] = news_times_list
-            df['news_media'] = news_medias_list
-
-            df_tot = pd.concat([df_tot,df], ignore_index=True)
-            # print(len(df))
-            # print(df)
-            
-
-        else:
-            print("웹 페이지에 접근할 수 없습니다. HTTP 상태 코드:", response.status_code)
-
-    # print(df_tot)
-    # print(len(df_tot))
-
-    df_tot.to_sql(table_name, engine, if_exists='replace', index=False)
-
-    engine.dispose()
-
-    # # Create a new News record and add it to the database
-    # db_news = models.News(**news_item.dict())
-    # db.add(db_news)
-    # db.commit()
-    # db.refresh(db_news)
-
-    return
-
-# Function to get the database session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# Function to initialize the database tables
-def init_db():
-    models.Base.metadata.create_all(bind=engine)
-
-# Create a scheduler instance
-scheduler = BackgroundScheduler()
-
-# Define a function to be scheduled
-def scrape_and_store_news_scheduler():
-    table_name = 'news'
-    print(table_name)
-
-    keywords = [
-        '전기,전자', '건설업', '운수장비', '의료정밀', '섬유,의복',
-        '통신업', '제조업', '의약품', '서비스업', '화학', '비금속광물',
-        '금융업', '음식료품', '종이,목재', '기계', '철강및금속', '운수창고', '유통업', '전기가스업'
-    ]
-
-    end_date = datetime.now().strftime("%Y%m%d")
-    start_date = (datetime.now() - timedelta(days=3)).strftime("%Y%m%d")
-    df_tot = pd.DataFrame()
-
-    for i in keywords[:10]:
-        search_query = i
-
-        url = f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_query}&sort=RELEVANT&ds={start_date}&de={end_date}"
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
-
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
 
         if response.status_code == 200:
+            logger.info(f"News Crawl {search_query} Started")
             df = pd.DataFrame()
             news_data = []
-            news_type = [search_query] * 10
-            # BeautifulSoup을 사용하여 HTML 파싱을 수행합니다.
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # "group_news" div 태그를 찾습니다.
+            # "group_news" div 태그 찾기
             group_news_div = soup.find('div', 'group_news')
             news_medias = group_news_div.find_all('a','info press')
             news_times = group_news_div.find_all('span','info')
+            news_image_urls = group_news_div.find_all('div', 'news_wrap api_ani_send')
 
             news_medias_list = []
             news_times_list = []
-            
+            news_image_urls_res = []
+
+            for news_image_url in news_image_urls:
+                test = news_image_url.find('a','dsc_thumb')
+                if test:
+                    image = test.find('img','thumb api_get')
+                    image = image['data-lazysrc']
+                    news_image_urls_res.append(image)
+                else:
+                    news_image_urls_res.append(None)
+
             for news_media in news_medias:
                 x = news_media.text.strip()
                 news_medias_list.append(x)
@@ -253,126 +81,102 @@ def scrape_and_store_news_scheduler():
                 else:
                     news_times_list.append(x)
 
+            # 3일전 / 2시간 전 형식 datetime으로 변환
             current_date = datetime.now()
-
-            # 변환된 결과를 저장할 리스트를 초기화합니다.
             converted_dates = []
             d = news_times_list
 
-            # 주어진 문자열 리스트를 순회하면서 datetime으로 변환합니다.
             for date_str in d:
                 if '전' in date_str:  # "3일 전" 또는 "3시간 전" 형식인 경우
                     date_split = date_str.split()
-                    match = re.search(r'\d+', date_str)  # 숫자 패턴을 찾습니다.
+                    match = re.search(r'\d+', date_str)  # 숫자 패턴 선언
                     if match:
-                        time_value = int(match.group())  # 숫자를 추출하고 정수로 변환합니다.
-                    # 시간 단위와 값 추출
-
-                    match = re.search(r'(시간|일)', date_split[0])  # "시간" 또는 "일" 패턴을 찾습니다.
+                        time_value = int(match.group())  # 숫자만 추출, 정수로 변환
+                    
+                    # 시간 단위 추출
+                    match = re.search(r'(분|시간|일)', date_split[0])  # "분","시간","일" 패턴 찾기
                     if match:
-                        time_unit = match.group()  # "시간" 또는 "일"을 추출합니다.
+                        time_unit = match.group()  # 패턴 추출
+                        if time_unit == '일':
+                            converted_date = current_date - timedelta(days=time_value)
+                        elif time_unit == '시간':
+                            converted_date = current_date - timedelta(hours=time_value)
+                        elif time_unit == '분':
+                            converted_date = current_date - timedelta(minutes=time_value)
+                        else:
+                            # 예외 사항
+                            converted_date = None
 
-                    if time_unit == '일':
-                        converted_date = current_date - timedelta(days=time_value)
-                    elif time_unit == '시간':
-                        converted_date = current_date - timedelta(hours=time_value)
-                    else:
-                        # 처리하지 않은 다른 시간 단위에 대한 로직을 추가하세요.
-                        # 예를 들어, '분' 또는 '주' 등을 처리할 수 있습니다.
-                        converted_date = None
-
-                    if converted_date:
                         converted_dates.append(converted_date)
                 else:  # "2023.09.01."과 같은 형식인 경우
                     try:
                         converted_date = datetime.strptime(date_str, "%Y.%m.%d.")
                         converted_dates.append(converted_date)
                     except ValueError:
-                        # 날짜 형식이 잘못된 경우 처리할 수 있습니다.
+                        # 날짜 형식 예외 사항 : 없음
                         pass
 
             news_times_list = converted_dates
 
-            # 뉴스 리스트를 포함한 하위 요소들을 가져옵니다.
+            # 뉴스 리스트를 포함한 하위 요소 추출
             news_items = group_news_div.find_all('a','news_tit')
-            # print("=====")
-            # print(news_items)
             for news_item in news_items:
                 # 기사 제목 추출
                 news_title = news_item.text.strip()
-
                 # 기사 URL 추출
                 news_url = news_item['href']
-
-                # 이미지 데이터 추출
-                news_image = news_item.find_previous('img', class_='thumb api_get')
-                if news_image:
-                    news_image_url = news_image.get('data-lazysrc')
-                else:
-                    news_image_url = None
-
-                # 결과를 딕셔너리로 저장하고 리스트에 추가합니다.
+                # 결과를 딕셔너리로 저장하고 리스트에 추가
                 news_item_data = {
                     'title': news_title,
-                    'url': news_url,
-                    'image_url': news_image_url
+                    'url': news_url
+                    # 'image_url': news_image_url
                 }
                 news_data.append(news_item_data)
             
             news_titles = []
             news_urls = []
-            news_image_urls = []
-            # 추출된 뉴스 데이터 출력 또는 활용
+
+            # 추출된 뉴스 데이터 리스트에 append
             for news_item in news_data:
                 news_titles.append(news_item['title'])
                 news_urls.append(news_item['url'])
-                news_image_urls.append(news_item['image_url'])
 
-            df['news_title'] = news_titles
-            df['news_url'] = news_urls
-            df['news_image_url'] = news_image_urls
-            df['news_type'] = [search_query] * 10
-            df['news_times'] = news_times_list
-            df['news_media'] = news_medias_list
+
+            # 데이터 프레임으로 정리
+            df['title'] = news_titles
+            df['origin_url'] = news_urls
+            df['image_url'] = news_image_urls_res
+            df['industry_id'] = [j] * 10
+            df['times'] = news_times_list
+            df['media'] = news_medias_list
 
             df_tot = pd.concat([df_tot,df], ignore_index=True)
-            # print(len(df))
-            # print(df)
-            time.sleep(0.3)
+            
+            time.sleep(10)
+
+            j += 1
+            logger.info(f"News Crawl {search_query} Completed")
         else:
-            print(url)
-            print("웹 페이지에 접근할 수 없습니다. HTTP 상태 코드:", response.status_code)
+            logger.error(f"News Crawl {search_query} Failed / {response.status_code}")
 
-    # print(df_tot)
-    # print(len(df_tot))
-
-    df_tot.to_sql(table_name, engine, if_exists='replace', index=False)
-    print("db 저장됨")
-    engine.dispose()
-
-    # # Create a new News record and add it to the database
-    # db_news = models.News(**news_item.dict())
-    # db.add(db_news)
-    # db.commit()
-    # db.refresh(db_news)
+    logger.info("DB Update Started")
+    db = SessionLocal()
+    for index, row in df_tot.iterrows():
+        news_item = News(
+            title=row['title'],
+            origin_url=row['origin_url'],
+            image_url=row['image_url'],
+            industry_id=row['industry_id'],
+            times=row['times'],
+            media=row['media']
+        )
+        db.add(news_item)
+    
+    db.commit()
+    
+    db.close()
+    logger.info("DB Update Completed")
     return
 
-
-
-# scheduler.add_job(scrape_and_store_news_scheduler, 'interval', minutes = 5)
-
-# Start the scheduler
-
-
-@router.post("/news/", response_model=NewsCreate)
-def create_news(news_item: NewsCreate, db: Session = Depends(get_db)):
-    return scrape_and_store_news(db, news_item)
-
-# Function to get a list of news from the database
-def get_news_list(db: Session, skip: int = 0, limit: int = 10):
-    news = db.query(models.News).offset(skip).limit(limit).all()
-    return news
-
-@router.get("/news/", response_model=list[NewsCreate])
-def read_news(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return get_news_list(db, skip, limit)
+def start_scheduler():
+    sched.start()
--- backEnd/newscrawl-server/app/main.py
+++ backEnd/newscrawl-server/app/main.py
@@ -1,24 +1,12 @@
 from fastapi import FastAPI
-from api.news import scrape_and_store_news_scheduler
-from db import init_db
-from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
-
-load_dotenv()
+from api.news import start_scheduler
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
-# Initialize the database
-init_db()
-
-# # Include the news router
-# app.include_router(scrape_and_store_news.router)
+start_scheduler()
 
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    scheduler.start()
-
-    # Add a job to the scheduler that runs every hour
-    scheduler.add_job(scrape_and_store_news_scheduler, 'interval', hours = 1)
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
