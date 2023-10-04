58275ac09cd6122ad502b9efacdeccc6fc295010
송준우 <boaz42@khu.ac.kr>
2023-10-04 14:50:24 +0900

    FEATURE<Client>: add create portfolio logic
    Merge: Merge branch 'FE/feature/createPort' into 'develop'
    
    * FEAT: add create portfolio logic
    
    * FEAT: create portFolio
    
    
    See merge request s09-fintech-finance-sub2/S09P22A309!46

--- frontEnd/junear/src/pages/Portfolio/CreatePort.tsx
+++ frontEnd/junear/src/pages/Portfolio/CreatePort.tsx
@@ -1,17 +1,17 @@
 import AddPortInfo from 'components/modal/AddPortInfo';
 import { useState } from 'react';
-
+import CreatePortSearch from 'components/input/CreatePortSearch';
+import axiosInstance from 'state/AxiosInterceptor';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Delete } from '../../assets/image/delete.svg';
 function CreatePort() {
+  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [corpName, setCorpName] = useState<string>('');
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<any[]>([]);
   // 각 검색 결과 항목에 대한 모달 데이터를 배열로 관리합니다.
-  const [modalData, setModalData] = useState<{ name: string; quantity: string; averagePrice: string }[]>(
-    Array(searchResults.length).fill({ name: '', quantity: '0', averagePrice: '0' }),
-  );
+  const [modalData, setModalData] = useState<any[]>([]);
   const [selectedResultIndex, setSelectedResultIndex] = useState<number | null>(null);
   const [selectedResultName, setSelectedResultName] = useState<string | null>(null);
-
   const openModal = (index: number) => {
     setIsModalOpen(true);
     // 모달을 열 때 선택한 검색 결과 항목의 인덱스와 이름을 설정합니다.
@@ -22,39 +22,51 @@ function CreatePort() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
+  const handleSearchResultsChange = (results: any[]) => {
+    setSearchResults(results);
+  };
   const handleModalBackgroundClick = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
       closeModal();
     }
   };
 
-  const handleCorpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCorpName(e.target.value);
-  };
-
-  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      console.log('Entered corpName:', corpName);
-      const results = ['검색 결과 1', '검색 결과 2', '검색 결과 3'];
-      setSearchResults(results);
-      setCorpName('');
-    }
-  };
-
   // 모달에서 데이터를 추가할 때 선택한 검색 결과 항목의 인덱스를 사용하여 모달 데이터를 업데이트합니다.
   const handleModalData = (data: { quantity: string; averagePrice: string }) => {
     if (selectedResultIndex !== null) {
-      // 모달 데이터를 추가하면서 해당 데이터를 목록의 선택한 인덱스에 위치시킵니다.
+      // 모달 데이터를 업데이트할 때 해당 인덱스의 요소를 직접 수정합니다.
       setModalData((prevModalData) => {
         const updatedModalData = [...prevModalData];
-        updatedModalData.splice(selectedResultIndex, 1, {
+        updatedModalData[selectedResultIndex] = {
           name: selectedResultName || '', // 이름 추가
           ...data,
-        });
+        };
         return updatedModalData;
       });
-      setSearchResults([]);
+    }
+  };
+  const handleDeleteClick = (indexToDelete: number) => {
+    setModalData((prevModalData) => {
+      // 선택한 인덱스를 제외한 새로운 배열을 생성합니다.
+      const updatedModalData = prevModalData.filter((_, index) => index !== indexToDelete);
+      return updatedModalData;
+    });
+  };
+  const createPort = async () => {
+    try {
+      const requestData = {
+        request_list: modalData.map((item) => ({
+          corporation_id: item.name.corporation_id,
+          stock_count: Number(item.quantity),
+          average_price: Number(item.averagePrice),
+          ...item,
+        })),
+      };
+      const response = await axiosInstance.post('/portfolio/init', requestData);
+      console.log(response);
+      navigate('/my-portfolio');
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
   };
 
@@ -62,34 +74,54 @@ function CreatePort() {
     <>
       <div className="col-span-12 text-white mt-[125px] text-[28px] text-center">보유중인 주식을 추가해 주십시오.</div>
       <div className="col-span-12 mt-[5px]"></div>
-      <div className="col-start-1 col-end-6 h-[420px] bg-zinc-700 rounded-[20px]">
-        <input
-          type="text"
-          placeholder="기업을 입력하세요."
-          value={corpName}
-          onChange={handleCorpChange}
-          onKeyDown={handleInputKeyDown}
-        />
+      <div
+        className="col-start-1 col-end-6 h-[420px] bg-zinc-700 rounded-[20px]"
+        style={{ overflow: 'auto', maxHeight: '420px' }}
+      >
+        <CreatePortSearch onSearchResultsChange={handleSearchResultsChange} />
         {searchResults.map((result, index) => (
-          <div className="pt-[25px] pb-[25px] border-b-2" key={index} onClick={() => openModal(index)}>
-            {result}
-            <div>{modalData[index]?.quantity}</div>
-            <div>{modalData[index]?.averagePrice}</div>
+          <div
+            className="m-2 text-[16px] pt-[20px] pb-[20px] text-white cursor-pointer
+          hover:bg-gray-800"
+            key={index}
+            onClick={() => openModal(index)}
+          >
+            {result.name}
           </div>
         ))}
       </div>
       <div className="col-start-8 col-end-13 h-[420px] bg-zinc-700 rounded-[20px]">
         {modalData.map((data, index) => (
-          <div className="pt-[25px] pb-[25px] border-b-2" key={index}>
-            {data.name} {/* 기업 이름 표시 */}
-            <div>{data.quantity}</div>
-            <div>{data.averagePrice}</div>
+          <div className="relative m-2 text-[16px] pt-[20px] pb-[20px] text-white" key={index}>
+            <div className="flex">
+              <div className="text-left">
+                {data.name.name}
+                <div style={{ color: '#B0B2B5' }}>{data.name.industry_type}</div>
+              </div>
+              <div className="flex text-left pl-[115px]">
+                <div className="absolute left-[200px]">
+                  <div>수량</div>
+                  <div>평단가</div>
+                </div>
+                <div className="absolute left-[270px]">
+                  <div>{data.quantity}</div>
+                  <div>{data.averagePrice}</div>
+                </div>
+              </div>
+              <Delete
+                className="absolute right-[20px] top-[27px] cursor-pointer"
+                onClick={() => handleDeleteClick(index)}
+              />
+            </div>
           </div>
         ))}
       </div>
       <div className="col-span-12 mt-[5px]"></div>
       <div className="relative col-span-12">
-        <button className="absolute text-white bg-teal-500 rounded-[20px] p-[14px] w-[250px] whitespace-nowrap left-[430px]">
+        <button
+          className="absolute text-white bg-teal-500 rounded-[20px] p-[14px] w-[250px] whitespace-nowrap left-[430px]"
+          onClick={createPort}
+        >
           포트폴리오 생성하기
         </button>
       </div>
