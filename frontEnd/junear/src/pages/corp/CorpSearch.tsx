3933ef9faa5f9f1e397585e6d210ad61a489a6a5
HBumzz <choi12920@gmail.com>
2023-10-06 02:16:27 +0900

    FIX: searchcomponent size fix

--- frontEnd/junear/src/pages/corp/CorpSearch.tsx
+++ frontEnd/junear/src/pages/corp/CorpSearch.tsx
@@ -1,3 +1,4 @@
+import { hover } from '@testing-library/user-event/dist/hover';
 import SearchPlaceHolder from 'components/input/SearchPlaceHolder';
 
 import { useState } from 'react';
@@ -9,6 +10,7 @@ function CorpSearch() {
   const handleSearchResultsChange = (results: any[]) => {
     setSearchResults(results);
   };
+  console.log(searchResults);
 
   const navigateToDetailPage = (corp_id: number) => {
     // 클릭한 기업 페이지로 이동
@@ -16,20 +18,20 @@ function CorpSearch() {
   };
   return (
     <>
-      <div className="mt-[50px]"></div>
-      <div className="col-start-1 col-end-13 ml-[5px] mt-[100px]">
+      <div className="col-span-3 mt-[50px]"></div>
+      <div className="col-start-1 col-end-13 ml-[5px] mt-[100px] mb-[20px]">
         <div className="text-white text-[28px] text-left text-center">원하시는 기업을 검색해 주세요.</div>
       </div>
-      <div className="col-start-1 col-end-13 flex relative justify-center">
+      <div className="col-start-1 col-end-13 h-[57px] flex relative justify-center">
         <SearchPlaceHolder onSearchResultsChange={handleSearchResultsChange} />
       </div>
       <div className="col-start-1 col-end-13 flex justify-center ">
         <div className="w-[376px] h-[422px] bg-zinc-700 rounded-[10px] border-b border-neutral-400 overflow-auto">
-          <div className="left-[17px] top-[27px] flex-col justify-start items-start gap-7 inline-flex">
+          <div className=" left-[17px] top-[27px] flex-col justify-start items-start gap-7 inline-flex">
             <div className="">
               {searchResults.map((result, index) => (
                 <div
-                  className="m-4 text-[18px] text-white cursor-pointer hover:bg-gray-800"
+                  className="m-2 text-[16px] text-white cursor-pointer hover:bg-gray-800"
                   key={index}
                   onClick={() => navigateToDetailPage(result.corporation_id)}
                   style={{}}
