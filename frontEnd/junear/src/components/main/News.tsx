3cc0d8e7cb388f691e3a2079db0bdf1763d9b2f9
송준우 <boaz42@khu.ac.kr>
2023-10-06 10:02:54 +0900

    STYLE: add Favicon

--- frontEnd/junear/src/components/main/News.tsx
+++ frontEnd/junear/src/components/main/News.tsx
@@ -6,20 +6,17 @@ interface NewsData {
   times: string;
   media: string;
 }
+
 function News({ title, industry, origin_url, image_url, times, media }: NewsData) {
-  const handleNewsClick = () => {
-    // 클릭 시 origin_url로 이동
-    window.location.href = origin_url;
-  };
   return (
-    <div className="h-[360px]  w-[250px] ">
-      <button>
-        <img src={image_url} alt="JUNEAR-3-2" className="h-[200px] w-[100%] rounded-[20px]" onClick={handleNewsClick} />
-      </button>
+    <div className="h-[360px] w-[250px]">
+      <a href={origin_url} target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
+        <img src={image_url} alt="JUNEAR-3-2" className="h-[200px] w-[100%] rounded-[20px]" />
+      </a>
       <div className="text-white mt-[20px]">
-        <button className="mt-[10px]" onClick={handleNewsClick}>
+        <a href={origin_url} target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
           {title}
-        </button>
+        </a>
         <div className="flex whitespace-nowrap text-xs mt-[20px] text-gray-400 gap-[20px] justify-center">
           <p>{industry}</p>
           <p>{media}</p>
@@ -29,4 +26,5 @@ function News({ title, industry, origin_url, image_url, times, media }: NewsData
     </div>
   );
 }
+
 export default News;
